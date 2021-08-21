1e6531140c99072c9a06664f60032d9e42a77c2e
Zeno Dalla Valle <zenodallavalle@gmail.com>
2021-08-21 16:01:27 +0200

    Time: 38 ms (92.52%), Space: 14.3 MB (27.62%) - LeetHub

--- roman-to-integer/roman-to-integer.py
+++ roman-to-integer/roman-to-integer.py
@@ -1,7 +1,7 @@
+dictionary = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
 class Solution:
     def romanToInt(self, s: str) -> int:
         tot = 0
-        dictionary = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
         for i in range(1, len(s)):
             next_value = dictionary[s[i]]
             current_value = dictionary[s[i-1]]
