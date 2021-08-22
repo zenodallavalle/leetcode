3e6ace772f0f57ef55f0ab2e84fbde4fa62c5c4e
Zeno Dalla Valle <zenodallavalle@gmail.com>
2021-08-22 11:37:03 +0200

    Time: 68 ms (39.08%), Space: 14.3 MB (16.80%) - LeetHub

--- palindrome-number/palindrome-number.py
+++ palindrome-number/palindrome-number.py
@@ -1,11 +1,14 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x<0:
-            return False
-        elif x < 10:
-            return True
-        rev_x = 0
-        n_digit = log10(x) // 1
-        for i in range(int(n_digit) + 1):
-            rev_x += ((x//10**(n_digit-i))%10)*(10**i)
-        return rev_x == x
+        if x < 0: return False
+    
+        div = 1
+        while x >= 10*div:
+            div *= 10
+
+        while x:
+            if (x % 10) != (x//div): return False
+            
+            x = (x % div) // 10
+            div /= 100
+        return True
