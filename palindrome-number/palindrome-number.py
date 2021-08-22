665022dab234e603ce6dba31c50ef0668168347d
Zeno Dalla Valle <zenodallavalle@gmail.com>
2021-08-22 11:42:23 +0200

    Time: 72 ms (29.08%), Space: 14.2 MB (49.36%) - LeetHub

--- palindrome-number/palindrome-number.py
+++ palindrome-number/palindrome-number.py
@@ -1,12 +1,18 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0: return False
-    
-        div = 1
-        while x >= 10*div:
-            div *= 10
-
+        elif x == 0: return True
+        
+        div = 10 ** (log10(x)//1)
+        
+        # div = 1
+        # while x >= 10*div:
+        #     div *= 10
+        
         while x:
+            # right = x % 10
+            # left = x//div
+            
             if (x % 10) != (x//div): return False
             
             x = (x % div) // 10
