b3d3c7e11264eaf011bfe493ec6e26e7a6977d8b
Zeno Dalla Valle <zenodallavalle@gmail.com>
2021-08-22 16:13:46 +0200

    Time: 76 ms (27.80%), Space: 14.3 MB (73.42%) - LeetHub

--- add-two-numbers/add-two-numbers.py
+++ add-two-numbers/add-two-numbers.py
@@ -5,19 +5,19 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        def sum(x, y, result=None, res=None, prev_r=0):
-            summed = x.val + y.val + prev_r
-            r = 0
+        def sum(x, y, result=None, res=None, prev_residual=0):
+            summed = x.val + y.val + prev_residual
+            residual = 0
             if summed > 9:
-                r += 1
+                residual += 1
                 summed -= 10
             if not result:
                 result = res = ListNode(summed)
             else:
                 res.next = ListNode(summed)
                 res = res.next
-            if (not x.next) and (not y.next) and (not r):
+            if (not x.next) and (not y.next) and (not residual):
                 return result
             else:
-                return sum(x.next or ListNode(), y.next or ListNode(), result, res, r)
+                return sum(x.next or ListNode(), y.next or ListNode(), result, res, residual)
         return sum(l1, l2)
