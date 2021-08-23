16bb4b0b719567817737ad5fec64666ee53c956a
Zeno Dalla Valle <zenodallavalle@gmail.com>
2021-08-23 12:44:45 +0200

    Time: 216 ms (5.03%), Space: 14.6 MB (25.89%) - LeetHub

--- longest-common-prefix/longest-common-prefix.py
+++ longest-common-prefix/longest-common-prefix.py
@@ -1,5 +1,6 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        
         def hasPrefix(s:str, prefix:str)->bool:
             for i, c in enumerate(prefix):
                 if i >= len(s):
@@ -7,10 +8,23 @@ class Solution:
                 if s[i] != c:
                     return False
             return True
-        strs = sorted(strs, key=len)
-        substring = strs[0]
+        
+        if len(strs)==1:return strs[0]
+        
+        j=1
+        if strs[0] == '':
+            return ''
+        else:
+            substring=strs[0][:j]
+        
         while substring:
-            if all(map(lambda x: hasPrefix(x, substring), strs)):
+            i = 1
+            while i<len(strs):
+                if not hasPrefix(strs[i], substring):
+                    return substring[:-1]
+                i+=1
+            if j == len(strs[0]):
                 return substring
-            substring = substring[:-1]
+            j += 1
+            substring = strs[0][:j]
         return ''
