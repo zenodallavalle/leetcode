0b198d5afe49c66284b3687ef122956d52868d9c
Zeno Dalla Valle <zenodallavalle@gmail.com>
2021-08-23 21:26:41 +0200

    Time: 24 ms (95.43%), Space: 14.3 MB (35.32%) - LeetHub

--- valid-parentheses/valid-parentheses.py
+++ valid-parentheses/valid-parentheses.py
@@ -1,18 +1,18 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        parentheses = []
+        parentheses = ''
         for c in s:
             if c in ['{', '[', '(']:
-                parentheses.append(c)
+                parentheses += c
             else:
                 if len(parentheses) == 0:
                     return False
                 elif c == '}' and '{' == parentheses[-1]:
-                    parentheses.pop()
+                    parentheses = parentheses[:-1]
                 elif c == ']' and '[' == parentheses[-1]:
-                    parentheses.pop()
+                    parentheses = parentheses[:-1]
                 elif c == ')' and '(' == parentheses[-1]:
-                    parentheses.pop()
+                    parentheses = parentheses[:-1]
                 else:
                     return False
         return len(parentheses) == 0
