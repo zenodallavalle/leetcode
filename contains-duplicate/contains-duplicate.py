863c84fb315cc6ef067341380476a54f98ab6211
Zeno Dalla Valle <zenodallavalle@gmail.com>
2021-08-24 17:42:44 +0200

    Time: 116 ms (78.86%), Space: 21.5 MB (18.49%) - LeetHub

--- contains-duplicate/contains-duplicate.py
+++ contains-duplicate/contains-duplicate.py
@@ -1,9 +1,18 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         # First solution time O(nlog(n)) space O(1)
-        nums.sort()
-        i = 0
-        while (i+1) < len(nums):
-            if nums[i] == nums[i+1]:return True
-            i+=1
+        # nums.sort()
+        # i = 0
+        # while (i+1) < len(nums):
+        #     if nums[i] == nums[i+1]:return True
+        #     i+=1
+        # return False
+        
+        # Second solution time O(N) space O(N)
+        ht = {}
+        for n in nums:
+            if n in ht:
+                return True
+            else:
+                ht[n] = None
         return False
