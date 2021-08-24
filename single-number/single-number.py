bd70780c8dbe0311f82990e1a83349b957e753c8
Zeno Dalla Valle <zenodallavalle@gmail.com>
2021-08-24 16:53:02 +0200

    Time: 124 ms (93.08%), Space: 16.6 MB (84.95%) - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -1,9 +1,15 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        nums = sorted(nums)
-        i, j = 0,1
-        while j < len(nums):
-            if nums[i] != nums[j]:return nums[i]
-            i += 2
-            j += 2
-        return nums[-1]
+    # First solution I found
+    #
+    #     nums = sorted(nums)
+    #     i, j = 0,1
+    #     while j < len(nums):
+    #         if nums[i] != nums[j]:return nums[i]
+    #         i += 2
+    #         j += 2
+    #     return nums[-1]
+        res = 0
+        for n in nums:
+            res = res ^ n
+        return res
